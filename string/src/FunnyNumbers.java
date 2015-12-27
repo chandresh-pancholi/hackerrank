951bb293570fcd946ca7c9ca8d52d493c1a71a4b
chandresh pancholi <chandresh.pancholi@flipkart.com>
2015-12-28 02:24:54 +0530

    panagram code

---
+++ string/src/FunnyNumbers.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * Created by chandresh.pancholi on 27/12/15.
+ */
+public class FunnyNumbers {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+
+        int inputNumbers = in.nextInt();
+
+        int count = 0;
+
+        while (count < inputNumbers){
+            boolean flag = true;
+            String inputString = in.next();
+
+            String reversedString = new StringBuffer(inputString).reverse().toString();
+
+            for(int i = 0; i<inputString.length()-1; i++){
+                if(Math.abs(inputString.charAt(i+1) - inputString.charAt(i)) !=
+                        Math.abs(reversedString.charAt(i+1) - reversedString.charAt(i))){
+
+                    System.out.println("Not Funny");
+
+                    flag = false;
+
+                    break;
+                }
+            }
+            if (flag)
+                System.out.println("Funny");
+            count++;
+        }
+
+
+    }
+}
